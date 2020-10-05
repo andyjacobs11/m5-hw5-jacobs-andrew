d91f7acec340a60ab30cb5f0347a6a325987147c
Andrew-Stephen-Jacobs <andrewjacobs@ufl.edu>
2020-10-04 20:59:08 -0400

    Created var pizza

---
+++ main.js
@@ -0,0 +1,10 @@
+var pizza = {
+    crust  : "Thick",
+    cheese : "Mozzerella",
+    sauce  : "Extra",
+    addToppings : function() {
+      console.log('Please add pepperoni and sausage');
+    }
+  };
+
+
