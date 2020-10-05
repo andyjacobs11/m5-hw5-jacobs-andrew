042017195d8f979ebecac108cacfa2b288441be0
Andrew-Stephen-Jacobs <andrewjacobs@ufl.edu>
2020-10-04 21:18:12 -0400

    Create orderPizza function

--- main.js
+++ main.js
@@ -7,4 +7,11 @@ var pizza = {
     }
   };
 
-
+function orderPizza(myParam){
+    return pizza.crust + " " + pizza.cheese + " " + pizza.sauce + " " + addToppings;
+}
+//     console.log(pizza.crust);
+//     console.log(pizza.cheese);
+//     console.log(pizza.sauce);
+//     console.log(pizza.addToppings);
+// };
